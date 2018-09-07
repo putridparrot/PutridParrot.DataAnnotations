639e2bf4250a4f0f0243882597ea62b7374b6dd0
Mark Timmings <mark.timmings@mtbsoftware.co.uk>
2018-09-07 23:02:53 +0100

    Initial checkin with ExpectedAttribute

---
+++ PutridParrot.DataAnnotations/ExpectedAttribute.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+
+namespace PutridParrot.DataAnnotations
+{
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field | AttributeTargets.Parameter)]
+    public class ExpectedAttribute : ValidationAttribute
+    {
+        public ExpectedAttribute(object fromSet, bool generateErrorMessage = false)
+        {
+            FromSet = fromSet;
+            GenerateErrorMessage = generateErrorMessage;
+        }
+
+        public object FromSet { get; set; }
+        public bool GenerateErrorMessage { get; set; }
+
+        public override bool IsValid(object value)
+        {
+            var array = FromSet as object[];
+            var isValid = array?.Contains(value) ?? FromSet.Equals(value);
+            if (GenerateErrorMessage)
+            {
+                ErrorMessage = CreateErrorMessage();
+            }
+            return isValid;
+        }
+
+        private string CreateErrorMessage()
+        {
+            return FromSet is object[] array ?
+                $"Must be one of the following: {String.Join(", ", array.Select(a => a.ToString()))}" :
+                $"Must be value {FromSet}";
+        }
+    }
+
+}
---
+++ Tests.PutridParrot.DataAnnotations/ExpectedAttributeTests.cs
@@ -0,0 +1,54 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using FluentAssertions;
+using NUnit.Framework;
+using PutridParrot.DataAnnotations;
+
+namespace Tests.PutridParrot.DataAnnotations
+{
+    [ExcludeFromCodeCoverage]
+    [TestFixture]
+    public class ExpectedAttributeTests
+    {
+        [Test]
+        public void GenerateMessage_False_ExpectNull()
+        {
+            var attr = new ExpectedAttribute(new [] { "Danger", "Mouse" });
+
+            attr.IsValid("Penfold");
+            attr.ErrorMessage
+                .Should()
+                .BeNull();
+        }
+
+        [Test]
+        public void GenerateMessage_True_ExpectNonNull()
+        {
+            var attr = new ExpectedAttribute(new[] { "Danger", "Mouse" }, true);
+
+            attr.IsValid("Penfold");
+            attr.ErrorMessage
+                .Should()
+                .NotBeNull();
+        }
+
+        [Test]
+        public void IsValid_NotValid_ExpectFalse()
+        {
+            var attr = new ExpectedAttribute(new[] { "Danger", "Mouse" }, true);
+
+            attr.IsValid("Penfold")
+                .Should()
+                .BeFalse();
+        }
+
+        [Test]
+        public void IsValid_IsValid_ExpectFalse()
+        {
+            var attr = new ExpectedAttribute(new[] { "Danger", "Mouse" }, true);
+
+            attr.IsValid("Mouse")
+                .Should()
+                .BeTrue();
+        }
+    }
+}
